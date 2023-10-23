f405644343ee4606bf889f4025b4c427604a5b79
root <ohamdi776@gmail.com>
2023-10-23 06:48:07 -0700

    Fixed Imported libraries are alphabetically ordered problem

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Returns information about employee TODO list progress to a json file"""
+import json
 import requests
 import sys
-import json
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
