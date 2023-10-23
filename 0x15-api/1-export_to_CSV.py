daeb0a1f7572e459854acf4724c25dd1febded1b
root <ohamdi776@gmail.com>
2023-10-23 06:13:08 -0700

    Completed 1. Export to CSV task

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Returns information about employee TODO list progress"""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todo = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), mode="w", newline="") as csv_file:
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        [csv_writer.writerow([user_id, username, task.get("completed"),
+                              task.get("title")]) for task in todo]
