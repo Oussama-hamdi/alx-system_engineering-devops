1c8b3ed10d0ecda657177d3338553e57c38fff3e
root <ohamdi776@gmail.com>
2023-10-23 05:28:59 -0700

    Completed 0. Gather data from an API task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Returns information about employee TODO list progress"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    todo = requests.get(url + "todos", params={"userId": user_id}).json()
+    completed_tasks = [t.get("title") for t in todo if t.get("completed")]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed_tasks), len(todo)))
+
+    for task in completed_tasks:
+        print("\t {}".format(task))
